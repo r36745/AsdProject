381b91c10d03ac0d2f5b2679fd7ba65e2f23b02f
Steven Roseman <stevierose@fullsail.edu>
2013-08-15 16:16:44 -0400

    able to save to local storage and display

---
+++ js/json.js
@@ -0,0 +1,52 @@
+var json = {
+	"Bio1": {
+		"fname": ["First Name", "Steve"],
+		"lname": ["Last Name", "Poser"],
+		"cWeight": ["Weight", "136"],
+		"bgoals": ["Body Goal", "Tone"],
+		"notes": ["Bio Notes", "Test Notes"]
+	},
+
+
+	"Bio2": {
+		"fname": ["First Name", "Monty"],
+		"lname": ["Last Name", "Hall"],
+		"cWeight": ["Weight", "122"],
+		"bgoals": ["Body Goal", "Lose Weight"],
+		"notes": ["Bio Notes", "workout 6 days"]
+	},
+
+
+	"Bio3": {
+		"fname": ["First Name", "Kadiet"],
+		"lname": ["Last Name", "Poser"],
+		"cWeight": ["Weight", "136"],
+		"bgoals": ["Body Goal", "Maintain"],
+		"notes": ["Bio Notes", "Maintain"]
+	},
+
+
+	"Bio4": {
+		"fname": ["First Name", "Cash"],
+		"lname": ["Last Name", "Raft"],
+		"cWeight": ["Weight", "122"],
+		"bgoals": ["Body Goal", "Tone"],
+		"notes": ["Bio Notes", "Toning up is my goal"]
+	},
+
+	"Bio5": {
+		"fname": ["First Name", "nancy"],
+		"lname": ["Last Name", "Raft"],
+		"cWeight": ["Weight", "122"],
+		"bgoals": ["Body Goal", "Maintain"],
+		"notes": ["Bio Notes", "be consisten in workout"]
+	},
+	
+	"Bio6": {
+		"fname": ["First Name", "Joel"],
+		"lname": ["Last Name", "McNugget"],
+		"cWeight": ["Weight", "122"],
+		"bgoals": ["Body Goal", "Lose Weight"],
+		"notes": ["Bio Notes", "I want to lose ten lbs"]
+	}
+}
